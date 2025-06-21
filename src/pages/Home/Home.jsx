7f323efbc095f510d854f85e5351a7cb534be60c
Sumaiya Nishat <sumaiyanishat94@gmail.com>
2025-06-22 02:30:46 +0600

    Update banner component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
+import Banner from "../../Components/Banner/Banner";
+
 
 
 const Home = () => {
     return (
         <div>
-            Home page
+            
+
+            {/* banner componet*/}
+            <div className="w-full min-h-[850px] bg-[#f0f2f3] flex items-center justify-center rounded-b-3xl">
+                <Banner></Banner>
+            </div>
         </div>
     )
 }
