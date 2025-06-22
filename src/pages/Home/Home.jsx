1768b6514edcdaa3e4b7eae4f768ff809c41f0f3
Sumaiya Nishat <sumaiyanishat94@gmail.com>
2025-06-23 00:51:14 +0600

    add featured product layout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../../Components/Banner/Banner";
 import Brand from "../../Components/Brand/Brand";
 import Delivery from "../../Components/Delivery/Delivery";
+import Features from "../../Components/Features/Features";
 
 
 
@@ -18,11 +19,16 @@ const Home = () => {
             <div className="delivery_component w-full min-h-[#150px]">
                 <Delivery></Delivery>
             </div>
+
              {/* delivery componet*/}
              <div className="brand flex items-center h-[171px] justify-center w-full mt-8 mb-8">
                 <Brand></Brand>
+                </div>
 
-             </div>
+                {/* features componet*/}
+             <div className="w-full flex items-center justify-center mb-[80px]">
+                <Features></Features>
+                </div>
         </div>
     )
 }
